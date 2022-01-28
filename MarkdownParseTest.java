262b1986a5e7ad5a58c6fed28e0feea2b8d8f7d1
Kenry3 <88472170+Kenry3@users.noreply.github.com>
2022-01-27 19:51:05 -0800

    test

---
+++ MarkdownParseTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLink() throws IOException {
+        Path fileName = Path.of("/Users/kenry/Documents/GitHub/markdown-parse/test-file1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com","some-page.html"), links);
+    }
+
+}
